444221032c248ad20e7e073b98c480e3cef0fcd0
Hung-Hwa Lin <hunghwalin@gmail.com>
2023-11-18 23:47:44 -0800

    nextNeighborObj

--- Mag_Avalanche.jl
+++ Mag_Avalanche.jl
@@ -10,7 +10,55 @@ using DataStructures
 struct IdNextNeighborCnt
 	neighborCnt::Int64;
 	id::Base.RefValue{Int64};
-	hVal::Base.RefValue{Int64};
+	hVal::Base.RefValue{Float64};
+end
+
+struct SpinArray
+	arr::Array{Bool};
+	arrSh::CircShiftedArray{Bool};
+	idLst::CartesianIndices;
+	idLstSh::CircShiftedArray{<:CartesianIndex};
+	nDim::Int64;
+	
+	idLstSorted::Vector{<:CartesianIndex};
+	lnId::Int64;
+	
+	hLocArr::Array{Float};
+	
+	function SpinArray( sz::Int64, nDim::Int64 )
+		szLst = ntuple( x->sz, nDim );
+		arr = zeros( Bool, szLst );
+		hLocArr = zeros( Float64, szLst );
+		arrSh = generateArrShLst( arr );
+		idLst = CartesianIndices( arr );
+		idLstSh = generateArrShLst( idLst );
+		
+		lnId = length(idLst);
+		idLstSorted = Vector{CartesianIndex{nDim}}(undef,lnId);
+		for ii = 1 : lnId
+			idLstSorted[ii] = idLst[ii];
+		end
+		
+		new( arr, arrSh, idLst, idLstSh, nDim, idLstSorted, lnId );
+	end
+end
+
+IdNextNeighborCnt(neighborCnt::Int64) = IdNextNeighborCnt( neighborCnt, Ref(1), Ref(0.0) );
+IdNextNeighborCnt(neighborCnt::Int64, id::Int64) = IdNextNeighborCnt( neighborCnt, Ref(id), Ref(0.0) );
+
+function advanceNextNeighbor!( idNextNeighbor::IdNextNeighborCnt, idLstSorted::Vector{<:CartesianIndex}, hLocArr::Array{Float64}, neighborHAddLst::Array{Float64}, idLast::Int64 )
+	idNextNeighbor.id[] += 1;
+	if idNextNeighbor.id[] > idLast
+		return false;
+	else
+		refreshNextNeighborHval!( idNextNeighbor, idLstSorted, hLocArr, neighborHAddLst );
+	end
+	
+	return true;
+end
+
+function refreshNextNeighborHval!( idNextNeighbor::IdNextNeighborCnt, idLstSorted::Vector{<:CartesianIndex}, hLocArr::Array{Float64}, neighborHAddLst::Array{Float64} )
+	idNextNeighbor.hVal[] = hLocArr[idLstSorted[idNextNeighbor.id[]]] + neighborHAddLst[idNextNeighbor.neighborCnt];
 end
 
 function magAvalanche( sz::Int64; varH, nDim::Int64 = 2, startPos = false )
@@ -37,39 +85,32 @@ function magAvalanche( sz::Int64; varH, nDim::Int64 = 2, startPos = false )
 	lnId = length( idLst );
 	idLstSh = [ ShiftedArrays.circshift( idLst, ntuple( ii -> ii == iDim ? (-1)^iSh : 0, nDim ) ) for iDim = 1 : nDim, iSh = 1 : 2 ];
 	
-	# hAppl = -4;
-	# hHistLst = Vector{Float64}(hVal,1);
 	hHistLst = fill( Float64(hAppl), 1 );
-	# mVal = -1;
 	mHistLst = fill( Float64(mVal), 1 );
 	
 	idLstSorted = Vector{CartesianIndex{nDim}}(undef,lnId);
 	for ii in 1:lnId
 		idLstSorted[ii] = idLst[ii];
 	end
-	# @infiltrate
 	sort!( idLstSorted; rev = isSortRev, by = ( x -> hLocArr[x] ) );
 	
+	neighborHAddLst = [-2*nDim:2.0:2*nDim;];
 	lnNextId = 2*nDim + 1;
-	nextCntIdLst = [1:lnNextId;];
-	nextUpIdLst = ones(Int64, lnNextId);
-	nextUpHLst = zeros(lnNextId);
-	neighborHAddLst = [-2*nDim:2:2*nDim;];
-	 
-	for iNext = 1 : lnNextId
-		nextUpHLst[iNext] = hLocArr[idLstSorted[nextUpIdLst[iNext]]] + neighborHAddLst[iNext];
+	idStart = 1;
+	idNextNeighborLst = [ IdNextNeighborCnt(neighborCnt,idStart) for neighborCnt = 1 : lnNextId ];
+	for ii = 1 : lnNextId
+		refreshNextNeighborHval!( idNextNeighborLst[ii], idLstSorted, hLocArr, neighborHAddLst );
 	end
 	
-	nextCntIdHeap = BinaryHeap( Base.By( ii -> nextUpHLst[ii], hLocOrder ), nextCntIdLst );
+	nextIdHeap = BinaryHeap( Base.By( idNext -> idNext.hVal[], hLocOrder ), idNextNeighborLst );
 	
 	hLoc = 0;
 	flipLst = Vector{CartesianIndex{nDim}}(undef,0);
 	
 	maxNeighborCnt = 1;
-	while !isempty( nextCntIdHeap )
-		maxNeighborId = pop!( nextCntIdHeap );
-		pos = idLstSorted[ nextUpIdLst[maxNeighborId] ];
-		# @infiltrate
+	while !isempty( nextIdHeap )
+		idNeighborMax = pop!( nextIdHeap );
+		pos = idLstSorted[ idNeighborMax.id[] ];
 		
 		if spinArr[pos] == spinStart
 			neighborCntId = 1;
@@ -77,12 +118,11 @@ function magAvalanche( sz::Int64; varH, nDim::Int64 = 2, startPos = false )
 				neighborCntId += spinArrSh[iDim, iSh][pos];
 			end
 			
-			if neighborCntId == maxNeighborId
-				hAppl = -( hLocArr[pos] + neighborHAddLst[maxNeighborId] );
+			if neighborCntId == idNeighborMax.neighborCnt
+				hAppl = -( hLocArr[pos] + neighborHAddLst[idNeighborMax.neighborCnt] );
 				hHistLst = push!( hHistLst, hAppl );
 				push!( flipLst, pos );
 				flipPos = pos;
-				# @infiltrate
 				while !isempty( flipLst )
 					flipPos = pop!(flipLst);
 					if spinArr[flipPos] != spinStart
@@ -100,7 +140,7 @@ function magAvalanche( sz::Int64; varH, nDim::Int64 = 2, startPos = false )
 							neighborCntId += spinArrSh[iDimNxt, iShNxt][posSh];
 						end
 						hLocNext += neighborHAddLst[neighborCntId];
-						# if hLocNext >= 0
+						
 						if hLocIsTriggerFun( hLocNext )
 							push!( flipLst, posSh );
 						end
@@ -110,19 +150,25 @@ function magAvalanche( sz::Int64; varH, nDim::Int64 = 2, startPos = false )
 			end
 		end
 		
-		nextUpIdLst[maxNeighborId] += 1;
-		if nextUpIdLst[maxNeighborId] <= lnId 
-			nextUpHLst[maxNeighborId] = hLocArr[ idLstSorted[nextUpIdLst[maxNeighborId]] ] + neighborHAddLst[maxNeighborId];
-			push!( nextCntIdHeap, maxNeighborId );
+		if advanceNextNeighbor!( idNeighborMax, idLstSorted, hLocArr, neighborHAddLst, lnId )
+			push!( nextIdHeap, idNeighborMax );
 		end
 	end
-	# @infiltrate
 	
 	return mHistLst, hHistLst;
 end
 
+function calcNeighborCntId( pos::CartesianIndex, spinArrSh::Vector{<:AbstractArray{Int64}} )
+	neighborCntId = 1;
+	
+end
+
 function boolToPN( bVal )
 	return 2*bVal - 1;
+end
+
+function generateArrShLst( arr )
+	return [ ShiftedArrays.circshift( arr, ntuple( id -> id == iDim ? (-1)^iSh : 0 ) ) for iDim = 1 : ndims(arr), iSh = 1:2 ];
 end
 
 end
